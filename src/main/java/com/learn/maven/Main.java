84b60c548c9f11577a190bcb3170608888d44ae4
abhishek <abhishekchauhan10x@gmail.com>
2023-09-24 06:23:22 -0400

    testing dependency

--- src/main/java/com/learn/maven/Main.java
+++ src/main/java/com/learn/maven/Main.java
@@ -1,7 +1,11 @@
 package com.learn.maven;
 
+import com.test.depend.messageSource;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        messageSource message = new messageSource();
+        System.out.println(message.getMessage());
     }
 }
